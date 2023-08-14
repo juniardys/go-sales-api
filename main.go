aee4307afd6ef0294678da7761c63ab58c6fbf35
juniardys <juniardysetio@gmail.com>
2023-08-14 16:03:22 +0700

    ref: cleanup db code

--- main.go
+++ main.go
@@ -2,47 +2,10 @@ package main
 
 import (
 	"fmt"
-	"go-sales-api/models"
-	"os"
-
-	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-
+	db "go-sales-api/config"
 )
-
-var DB *gorm.DB
 
 func main() {
 	fmt.Println("Go sales api started...")
-
-	godotenv.Load()
-	dbhost := os.Getenv("MYSQL_HOST")
-	dbuser := os.Getenv("MYSQL_USER")
-	dbpassword := os.Getenv("MYSQL_PASSWORD")
-	dbname := os.Getenv("MYSQL_DBNAME")
-
-	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbuser, dbpassword, dbhost, dbname)
-	var db, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
-
-	if err != nil {
-		panic("db connection failed")
-	}
-
-	DB = db
-	fmt.Println("db connected successfully")
-
-	AutoMigrate(db)
-}
-
-func AutoMigrate(connection *gorm.DB) {
-	connection.Debug().AutoMigrate(
-		&models.Cashier{},
-		&models.Category{},
-		&models.Payment{},
-		&models.PaymentType{},
-		&models.Product{},
-		&models.Discount{},
-		&models.Order{},
-	)
+	db.Connect()
 }
