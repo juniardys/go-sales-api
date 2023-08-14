c901bbd4b3a6d9789a36d1bd992c91104323313e
juniardys <juuns99@gmail.com>
2023-08-14 09:25:28 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/test", func(ctx *fiber.Ctx) error {
+		return ctx.Status(200).JSON(fiber.Map{
+			"success": true,
+			"message": "Go fiber first api created",
+		})
+	})
+
+	app.Listen(":3000")
+}
