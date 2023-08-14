423dcb514565a4738a7f0b2dff91e311c9db036a
juniardys <juniardysetio@gmail.com>
2023-08-14 15:54:49 +0700

    feat: setup model

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+)
+
+var DB *gorm.DB
 
 func main() {
-	app := fiber.New()
+	fmt.Println("Go sales api started...")
 
-	app.Get("/test", func(ctx *fiber.Ctx) error {
-		return ctx.Status(200).JSON(fiber.Map{
-			"success": true,
-			"message": "Go fiber first api created",
-		})
-	})
+	godotenv.Load()
+	dbhost := os.Getenv("MYSQL_HOST")
+	dbuser := os.Getenv("MYSQL_USER")
+	dbpassword := os.Getenv("MYSQL_PASSWORD")
+	dbname := os.Getenv("MYSQL_DBNAME")
 
-	app.Listen(":3000")
+	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbuser, dbpassword, dbhost, dbname)
+	var db, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
+
+	if err != nil {
+		panic("db connection failed")
+	}
+
+	DB = db
+	fmt.Println("db connected successfully")
+}
+
+func AutoMigrate(connection *gorm.DB) {
+	connection.Debug().AutoMigrate(
+		&models
+	)
 }
